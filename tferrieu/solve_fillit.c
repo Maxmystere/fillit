b73ffea03481a70b7d29a6c2337a7ac1fb760803
Maxence GRAB <magrab@e2r2p11.42.fr>
2019-01-06 19:41:30 +0100

    Update tferrieu

---
+++ tferrieu/solve_fillit.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solve_fillit.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: magrab <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/06 16:52:24 by magrab            #+#    #+#             */
+/*   Updated: 2019/01/06 19:38:34 by magrab           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tetriminos.h"
+
+static int		try_fillit(char ***map, int size, t_tetri **tab, int curr_tetri)
+{
+	int		try;
+	int		x;
+	int		y;
+
+	try = -1;
+	if (!tab)
+		return (0);
+	x = 0;
+	y = 0;
+	while (try)
+	{
+		try = add_tetri(map, tab[curr_tetri], x, y);
+		if ((try = add_tetri(map, tab, x, y) == 0)) // a pu placer
+		{
+			if ((try = try_fillit(map, size, tab + 1, curr_tetri + 1)) == 0)
+				return (0);
+			remove_tetri(map, tab[curr_tetri], x, y);
+		}
+		x++;
+		if (x > size - (tab[0])->w)
+		{
+			x = 0;
+			if (++y > size - (tab[0])->h)
+				return (-1);
+		}
+	}
+}
+
+char		**solve_fillit(t_tetri **tab, int nb_tetri)
+{
+	int		size;
+	char	**map;
+	int		try;
+
+	try = 1;
+	size = 2;
+	while (size * size < nb_tetri * 4)
+		size++;
+	while (try)
+	{
+		if (!(map = ft_2dchar_make(size, size)))
+			return (NULL);
+		try = try_fillit(&map, size, tab, 0);
+		if (try && size++)
+			ft_2dchar_free(&map);
+	}
+	return (map);
+}
--- tferrieu/tetriminos.h
+++ tferrieu/tetriminos.h
@@ -6,21 +6,24 @@
 /*   By: tferrieu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/23 16:13:40 by tferrieu          #+#    #+#             */
-/*   Updated: 2019/01/06 17:02:34 by tferrieu         ###   ########.fr       */
+/*   Updated: 2019/01/06 18:05:32 by magrab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TETRIMINOS_H
 # define TETRIMINOS_H
 
+# include "libft/libft.h"
+
 typedef struct	s_tetri
 {
-	char	**shape;
-	int		h;
-	int		w;
-	int		status;
+	char		**shape;
+	int			h;
+	int			w;
+	int			status;
 }				t_tetri;
 
 int				check_fillit(char **buff, int fd, t_tetri ***tab);
+char			**solve_fillit(t_tetri **tab, int nb_tetri);
 
 #endif
